f8069597a9634084e3f4f20b0d5133e02bd53106
Ravindra Potdar <ravindrapotdar@Ravindras-MacBook-Pro.local>
2024-07-18 23:38:33 +0400

    Fixed layout issue in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-betweenxx p-24">
+      <h2>Welcome to Truegether</h2>
       <w3m-button />
     </main>
   );
